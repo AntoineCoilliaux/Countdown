e3f596ac45e163c64eda22baca0014d4a9a3ad73
Antoine Coilliaux <antoine.coilliaux@gmail.com>
2026-02-02 16:40:46 +0100

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Antoine Coilliaux on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
